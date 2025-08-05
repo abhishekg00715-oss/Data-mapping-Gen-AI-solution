555a32d22a74643fdbcf607f7bb1360c31f8eea6
abhishekg00715-oss <abhishekg00715@gmail.com>
2025-08-05 20:51:06 +0530

    Update placeholder.py

--- code/placeholder.py
+++ code/placeholder.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+
 df = pd.read_csv('/content/sample_data/data/mapping_metadata.csv')
 
 
@@ -30,6 +32,27 @@ def get_embedding(combined_text, model="all-MiniLM-L6-v2"):
 new_data = knowledge_base(df)
 embedding = get_embedding(new_data['combined_text'])
 print(embedding)
+
+
+# Example user query (dynamic) -- you can take input from chat
+def user_query(data,embedding,model="all-MiniLM-L6-v2"):
+
+  query = "How do I transform the order_amount field?"
+  model = SentenceTransformer(model)
+# Generate the embedding for the user query
+  query_embedding = model.encode(query)
+
+# Calculate cosine similarity between the query and the knowledge base embeddings
+  cos_similarities = cosine_similarity(query_embedding,embedding)
+
+# Find the index of the most similar knowledge base entry
+  most_similar_idx = cos_similarities.argmax()
+
+# Retrieve the most similar knowledge base entry (combined text)
+  most_similar_entry = data.iloc[most_similar_idx]['combined_text']
+  print(f"Most similar knowledge base entry: {most_similar_entry}")
+
+user_query(new_data,embedding)
 
 
 
